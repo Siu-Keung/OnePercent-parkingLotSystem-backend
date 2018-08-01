180159c5229d59f3f300d0f05e68ded446b446ba
SiuKeung <siu_keung@outlook.com>
2018-08-01 14:02:29 +0800

    增加了当id不存在时，查找停车场信息的方法是否会抛出异常

--- src/test/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImplTest.java
+++ src/test/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImplTest.java
@@ -1,17 +1,21 @@
 package com.onepercent.ParkingLotApplication.service.impl;
 
 import com.onepercent.ParkingLotApplication.domain.ParkingLot;
+import com.onepercent.ParkingLotApplication.exception.ResourceNotFoundException;
 import com.onepercent.ParkingLotApplication.repository.ParkingLotRepository;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.CoreMatchers.*;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,6 +27,9 @@ public class ParkingLotServiceImplTest {
     private ParkingLotServiceImpl parkingLotService;
 
     private ParkingLotRepository repository;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void beforeEach(){
@@ -39,6 +46,16 @@ public class ParkingLotServiceImplTest {
         ParkingLot parkingLot = this.parkingLotService.getParkingLotById(123L);
 
         assertThat(parkingLot, notNullValue());
+    }
+
+    @Test
+    public void should_throw_exception_given_id_not_exists() {
+        Optional optional = Optional.empty();
+
+        when(this.repository.findById(anyLong())).thenReturn(optional);
+
+        expectedException.expect(ResourceNotFoundException.class);
+        this.parkingLotService.getParkingLotById(1234L);
     }
 
 
