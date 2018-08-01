fb8e2eebcb98912456e1e73e67bb343eb1c7d9dc
SiuKeung <siu_keung@outlook.com>
2018-08-01 20:12:12 +0800

    correct the authentication

--- src/main/java/com/onepercent/ParkingLotApplication/controller/TestController.java
+++ src/main/java/com/onepercent/ParkingLotApplication/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.onepercent.ParkingLotApplication.controller;
 
+import com.onepercent.ParkingLotApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,16 @@ public class TestController {
     @RequestMapping("/")
     public String sayHello(){
         return "欢迎使用停车场管理系统！！！！！！";
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @PreAuthorize("hasAnyAuthority('Admin', 'Manage') ")
+    @RequestMapping("/test")
+    public Object test(){
+        return userRepository.findAll();
     }
 
 }
