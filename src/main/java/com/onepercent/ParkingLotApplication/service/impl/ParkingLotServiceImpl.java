8f786243c79046a7337d3a1c2ecddbae48b9fc9c
SiuKeung <siu_keung@outlook.com>
2018-08-01 13:52:41 +0800

    实现了根据指定id获取停车场的方法

--- src/main/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImpl.java
@@ -2,9 +2,13 @@ package com.onepercent.ParkingLotApplication.service.impl;
 
 import com.onepercent.ParkingLotApplication.domain.ParkingLot;
 import com.onepercent.ParkingLotApplication.exception.ResourceNotFoundException;
+import com.onepercent.ParkingLotApplication.repository.ParkingLotRepository;
 import com.onepercent.ParkingLotApplication.service.ParkingLotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Optional;
 
 /**
  * @author Dylan Wei
@@ -12,9 +16,18 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 @Service
 public class ParkingLotServiceImpl implements ParkingLotService {
+    private ParkingLotRepository parkingLotRepository;
+
+    @Autowired
+    public ParkingLotServiceImpl(ParkingLotRepository repository){
+        this.parkingLotRepository = repository;
+    }
 
     @Override
     public ParkingLot getParkingLotById(Long id) throws ResourceNotFoundException {
-        throw new NotImplementedException();
+        Optional<ParkingLot> optional = this.parkingLotRepository.findById(id);
+        if(!optional.isPresent())
+            throw new ResourceNotFoundException();
+        return optional.get();
     }
 }
--- src/test/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImplTest.java
+++ src/test/java/com/onepercent/ParkingLotApplication/service/impl/ParkingLotServiceImplTest.java
@@ -1,11 +1,45 @@
 package com.onepercent.ParkingLotApplication.service.impl;
 
-import static org.junit.Assert.*;
+import com.onepercent.ParkingLotApplication.domain.ParkingLot;
+import com.onepercent.ParkingLotApplication.repository.ParkingLotRepository;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Dylan Wei
  * @date 2018-08-01 13:18
  */
 public class ParkingLotServiceImplTest {
+    private ParkingLotServiceImpl parkingLotService;
+
+    private ParkingLotRepository repository;
+
+    @Before
+    public void beforeEach(){
+        this.repository = mock(ParkingLotRepository.class);
+        this.parkingLotService = new ParkingLotServiceImpl(this.repository);
+    }
+
+    @Test
+    public void should_get_specefic_parkinglot_given_valid_id(){
+        ParkingLot temp = new ParkingLot();
+        Optional optional = Optional.of(temp);
+        when(this.repository.findById(any())).thenReturn(optional);
+
+        ParkingLot parkingLot = this.parkingLotService.getParkingLotById(123L);
+
+        assertThat(parkingLot, notNullValue());
+    }
+
 
 }
