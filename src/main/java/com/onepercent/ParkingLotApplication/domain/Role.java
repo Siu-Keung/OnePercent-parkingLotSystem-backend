b1aca44f784659425ef635d8bbb974d9855fed8f
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-01 20:26:10 +0800

    add default Employee role and delete

--- src/main/java/com/onepercent/ParkingLotApplication/domain/Role.java
+++ src/main/java/com/onepercent/ParkingLotApplication/domain/Role.java
@@ -12,6 +12,11 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
 
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
--- src/main/java/com/onepercent/ParkingLotApplication/service/UserService.java
+++ src/main/java/com/onepercent/ParkingLotApplication/service/UserService.java
@@ -1,5 +1,6 @@
 package com.onepercent.ParkingLotApplication.service;
 
+import com.onepercent.ParkingLotApplication.domain.Role;
 import com.onepercent.ParkingLotApplication.domain.User;
 import com.onepercent.ParkingLotApplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 /**
  * @Author: Leon
@@ -22,6 +24,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public List findAll() {
+
         return userRepository.findAll();
     }
 
@@ -55,17 +58,41 @@ public class UserService {
     }
 
     public User save(User user) {
-        return userRepository.save(user);
+        Random random = new Random();
+        String randomPassword="";
+        String randomloginName="";
+        for (int i=0;i<6;i++)
+        {
+            randomPassword+=random.nextInt(10);
+            randomloginName+=random.nextInt(10);
+        }
+        user.setLoginName(randomloginName);
+        user.setPassword(randomPassword);
+        user.setLoginFlag("1");
+        if (user.getRoles()==null){
+            Role role=new Role(3,"Employee");
+            List<Role> roles=new ArrayList<>();
+            roles.add(role);
+            user.setRoles(roles);
+
+        }
+        userRepository.save(user);
+        return user;
     }
 
     public User update(int id, User user) {
         User oldUser=userRepository.findById(id).get();
         if (oldUser==null) return null;
-        oldUser.setName(user.getName());
-        oldUser.setEmail(user.getEmail());
-        oldUser.setPhone(user.getPhone());
-        oldUser.setLoginFlag(user.getLoginFlag());
-        oldUser.setDeleteFlag(user.getDeleteFlag());
+        if (user.getName()!=null)
+            oldUser.setName(user.getName());
+        if(user.getEmail()!=null)
+            oldUser.setEmail(user.getEmail());
+        if (user.getPhone()!=null)
+            oldUser.setPhone(user.getPhone());
+        if(user.getLoginFlag()!=null)
+            oldUser.setLoginFlag(user.getLoginFlag());
+        if(user.getRoles()!=null)
+            oldUser.setRoles(user.getRoles());
         userRepository.save(oldUser);
         return oldUser;
     }
