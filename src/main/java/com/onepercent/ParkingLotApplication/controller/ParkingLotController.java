d230d15968cfce3eeddaaa04ff2d24a0237f3a60
SiuKeung <siu_keung@outlook.com>
2018-08-01 16:13:11 +0800

    create a new controller named ParkingLotController, which is used to receive and handle the requests upon the parkinglot

---
+++ src/main/java/com/onepercent/ParkingLotApplication/controller/ParkingLotController.java
@@ -0,0 +1,17 @@
+package com.onepercent.ParkingLotApplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dylan Wei
+ * @date 2018-08-01 16:11
+ */
+@RestController
+@RequestMapping("/parkinglots")
+public class ParkingLotController {
+
+
+
+
+}
