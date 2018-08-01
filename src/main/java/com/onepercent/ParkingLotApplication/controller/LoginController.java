22b73e04b265040a441150af8be113648426a05f
XUHO3 <howells.xu@oocl.com>
2018-08-01 20:48:05 +0800

    login controllor return json

--- src/main/java/com/onepercent/ParkingLotApplication/controller/LoginController.java
+++ src/main/java/com/onepercent/ParkingLotApplication/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.onepercent.ParkingLotApplication.controller;
 
 import com.onepercent.ParkingLotApplication.config.WebSecurityConfig;
 import com.onepercent.ParkingLotApplication.dto.LoginDTO;
+import com.onepercent.ParkingLotApplication.dto.TokenDTO;
 import com.onepercent.ParkingLotApplication.repository.UserRepository;
 import com.onepercent.ParkingLotApplication.service.LoginService;
 import com.onepercent.ParkingLotApplication.utils.JWTTokenUtils;
@@ -19,29 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
-/**
- * Created by linyuan on 2017/12/13.
- */
+
 @RestController
 public class LoginController {
 
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private JWTTokenUtils jwtTokenUtils;
       @Autowired
       private LoginService loginService;
     @PostMapping("/auth/login")
-    public String login(@RequestBody LoginDTO loginDTO, HttpServletResponse httpResponse) throws Exception{
+    public TokenDTO login(@RequestBody LoginDTO loginDTO, HttpServletResponse httpResponse) throws Exception{
 
-       String tokenAndRole= loginService.login(loginDTO);
-       String token=tokenAndRole.split(" ")[0];
-       String role=tokenAndRole.split(" ")[1];
-       httpResponse.addHeader(WebSecurityConfig.AUTHORIZATION_HEADER,"Bearer "+token);
-        return tokenAndRole;
+        String tokenAndRole= loginService.login(loginDTO);
+        String token=tokenAndRole.split(" ")[0];
+        String role=tokenAndRole.split(" ")[1];
+        httpResponse.addHeader(WebSecurityConfig.AUTHORIZATION_HEADER,"Bearer "+token);
+        TokenDTO tokenDTO=new TokenDTO();
+        tokenDTO.setRole(role);
+        tokenDTO.setToken("Bearer "+token);
+        return   tokenDTO ;
     }
 }
