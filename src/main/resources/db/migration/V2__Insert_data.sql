c5e6548e4d99e04ab33836568cf1cf81fefc7b04
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-01 17:28:51 +0800

    write user_role mapping relation and create role table and user_role table

--- src/main/resources/db/migration/V2__Insert_data.sql
+++ src/main/resources/db/migration/V2__Insert_data.sql
@@ -1,5 +1,6 @@
 insert into user values(1, 'admin','admin', '$2a$10$UJW/Q1BGPctlUrLznCt/XOe/5zOL2TTpIaDxiEX.iA6fQKqc859jy','10@qq.com','110','1','1');
 insert into user values(2, 'admin1','admin1', '$2a$10$UJW/Q1BGPctlUrLznCt/XOe/5zOL2TTpIaDxiEX.iA6fQKqc859jy','11@qq.com','111','1','1');
+insert into user values(3, 'admin3','admin3', '$2a$10$UJW/Q1BGPctlUrLznCt/XOe/5zOL2TTpIaDxiEX.iA6fQKqc859jy','11@qq.com','111','1','1');
 
 
 insert into role values(1,'Admin');
