7fa76a8ad7beae1cccb860fe2eabd8255652c88b
SiuKeung <siu_keung@outlook.com>
2018-08-01 19:08:19 +0800

    correct the table's name

--- src/main/resources/db/migration/V1__tables.sql
+++ src/main/resources/db/migration/V1__tables.sql
@@ -1,4 +1,4 @@
-create table coordinator(
+create table user(
 id bigint auto_increment primary key,
 name varchar(100),
 password varchar(100)
--- src/test/java/com/onepercent/ParkingLotApplication/controller/ParkingLotControllerTest.java
+++ src/test/java/com/onepercent/ParkingLotApplication/controller/ParkingLotControllerTest.java
@@ -19,10 +19,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
